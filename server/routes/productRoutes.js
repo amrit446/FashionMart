e816c0c4d8919b5b3fdb8c3f811bb1db6417dfb1
amrit446 <hntr776@gmail.com>
2024-05-09 05:44:52 +0530

    create product api

---
+++ server/routes/productRoutes.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import {isAdmin, requireSignIn} from '../middleware/authMiddleware.js';
+import { createProductController, deleteProductController, getProductController, getSingleProductController, productPhotoController, updateProductController } from '../controllers/productController.js';
+import formidable from 'express-formidable';
+
+const router = express.Router()
+
+//routes
+router.post('/create-product', requireSignIn, isAdmin, formidable(),
+createProductController
+)
+
+
+//get products
+router.get('/get-product', getProductController);
+
+
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController)
+
+//delete product
+router.delete("/product/:pid", deleteProductController)
+
+
+//routes
+router.put(
+    "/update-product/:pid",
+    requireSignIn,
+    isAdmin,
+    formidable(),
+    updateProductController
+  );
+  
+
+export default router; 
